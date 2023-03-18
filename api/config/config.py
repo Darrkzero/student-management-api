35aade88b7a7392acaad0faec6719d378447af4d
Mgbemena Chimaobi <chimaobiephraim@yahoo.com>
2023-03-18 18:57:19 +0100

    first commit

--- api/config/config.py
+++ api/config/config.py
@@ -4,9 +4,7 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-uri = os.environ.get('DATABASE_URL') # or other relevant config var
-if uri and uri.startswith('postgres://'):
-    uri = uri.replace('postgres://', 'postgresql://', 1)
+
 
 # reading env files
 class Config:
@@ -30,7 +28,8 @@ class TestConfig(Config):
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = uri
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = config('DEBUG', False, cast=bool)
 
